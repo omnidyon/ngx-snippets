02fed5f0b2417a5e0b6ae79a86155adf28d5f1fa
Slavko Mihajlovic <slavkomihajl@protonmail.com>
2025-05-19 19:44:01 +0200

    Config correction

--- projects/ngx-snippets/src/public-api.ts
+++ projects/ngx-snippets/src/public-api.ts
@@ -4,9 +4,7 @@
  *
  * Use of this source code is governed by an ISC-style license that can be
  * found at https://www.isc.org/licenses/
- */
-
-/*
+ * 
  * Public API Surface of ngx-snippets
  */
 
